f0b348757062006989835ec9b2118ccbb1550819
rajat21186 <108051888+rajat21186@users.noreply.github.com>
2022-07-04 02:44:05 +0530

    Update SimpleAssembler.py

--- SimpleAssembler.py
+++ SimpleAssembler.py
@@ -12,14 +12,15 @@ for t in B:
 D=[] 
 dcv=dict()
 def dict():
-    dcr={"R0":"000","R1":"001","R2":"010","R3":"011","R4":"100","R5":"101","R6":"110"}
+    dcr={"R0":"000","R1":"001","R2":"010","R3":"011","R4":"100","R5":"101","R6":"110","FLAGS":"111"}
     dc={"add":"10000","sub":"10001","movI":"10010","movR":"10011","ld":"10100","st":"10101","mul":"10110","div":"10111","rs":"11000","ls":"11001","xor":"11010","or":"11011","and":"11100","not":"11101","cmp":"11110","jmp":"11111","jlt":"01100","jgt":"01101","je":"01111","hlt":"01010"}
     return dc,dcr
 def list():
     d=[]
     for t in range(7):
         s="R"+str(t)
-        d.append(s)       
+        d.append(s)
+    d.append("FLAGS")
     return d
 def var(B,j,beg,pc):
     c=B[j].split()
